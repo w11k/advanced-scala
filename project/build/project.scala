d677af6ff3729e054bdc7994af40d4eecfc622c9
Heiko Seeberger <heiko.seeberger@googlemail.com>
2011-04-14 18:24:05 +0200

    Continuations: Initial situation.

--- project/build/project.scala
+++ project/build/project.scala
@@ -1,6 +1,6 @@
 import sbt._
 
-class Project(info: ProjectInfo) extends DefaultProject(info) {
+class Project(info: ProjectInfo) extends DefaultProject(info) with AutoCompilerPlugins {
 
   // Repositories
   val scalaz51ModuleConfig = ModuleConfiguration("com.googlecode.scalaz", ScalaToolsSnapshots) // For scalaz 5.1-SNAPSHOT needed by specs2
@@ -10,4 +10,8 @@ class Project(info: ProjectInfo) extends DefaultProject(info) {
 
   // Test dependencies
   val specs2 = "org.specs2" %% "specs2" % "1.1" % "test" withSources
+
+  // Continuations
+  val cont = compilerPlugin("org.scala-lang.plugins" % "continuations" % buildScalaVersion)
+  override def compileOptions = super.compileOptions ++ compileOptions("-P:continuations:enable")
 }
